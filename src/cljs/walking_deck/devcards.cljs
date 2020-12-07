c55638141f7a9aba4a5f8d3adc9deef5cd302792
David <therabidbanana@gmail.com>
2020-12-07 05:57:36 +0000

    Fix devcards require

--- src/cljs/walking_deck/devcards.cljs
+++ src/cljs/walking_deck/devcards.cljs
@@ -1,8 +1,8 @@
 (ns walking-deck.devcards
   (:require
-   [reagent.core :as reagent]) ; just for example
-  (:require-macros
-   [devcards.core :refer [defcard]]))
+   [reagent.core :as reagent]
+   [devcards.core :refer-macros [defcard]]) ; just for example
+  )
 
 (defcard reagent-no-help
   (reagent/as-element [:h1 "Reagent example"]))

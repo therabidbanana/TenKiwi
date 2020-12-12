e20a5f4868126ee7c92f08ee5f3cfa48f1e14797
David <therabidbanana@gmail.com>
2020-12-12 22:40:25 +0000

    Update styles

--- src/clj/walking_deck/styles.clj
+++ src/clj/walking_deck/styles.clj
@@ -2,4 +2,12 @@
   (:require [garden-watcher.def :refer [defstyles]]))
 
 (defstyles style
-  [:h1 {:text-decoration "underline"}])
+  [:h1 {:text-decoration "underline"}]
+  [:input {:width "100%"
+           :font-size "2rem"
+           :margin "1rem"}]
+  [:form {:width "90%"
+          :margin "0 auto"}]
+  [:body {:color "#eeeef0"
+          :font-size "1.4em"
+          :background-color "#223"}])

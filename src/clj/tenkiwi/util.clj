90dde1a7668b048acaf238574804cdeebc920db8
David <therabidbanana@gmail.com>
2021-06-19 22:49:53 +0000

    Allow cards to be tagged

--- src/clj/tenkiwi/util.clj
+++ src/clj/tenkiwi/util.clj
@@ -8,6 +8,12 @@
         (list 'print "\n")
         'result))
 
+(defn update-values [m f & args]
+  (reduce (fn [r [k v]] (assoc r k (apply f v args))) {} m))
+
+(defn update-keys [m f & args]
+  (reduce (fn [r [k v]] (assoc r (apply f k args) v)) {} m))
+
 (defn read-spreadsheet-data
   ([url]
    (read-spreadsheet-data url second))
@@ -27,10 +33,27 @@
 (defn normalize-twospace [text]
   (clojure.string/replace text #"\s\s" "\n\n"))
 
+
+(defn extract-tags [{:as card
+                     :keys [text]}]
+  (let [[tag-line rest] (if (clojure.string/starts-with? text "#")
+                          (clojure.string/split text #"\n\n")
+                          ["" text])
+        tags
+        (reduce #(assoc %1 (clojure.string/replace %2 "#" "") true)
+                {}
+                (clojure.string/split tag-line #","))
+        ]
+    (assoc card
+           :tags (update-keys tags keyword)
+           :text rest)))
+
 (defn normalize-card [id map]
   (-> (assoc map :id id)
       (update :type keyword)
-      (update :text normalize-twospace)))
+      (update :text normalize-twospace)
+      (extract-tags)
+      ))
 
 (defn gather-decks [url]
   (let [cards (read-spreadsheet-data url normalize-card)]
@@ -40,12 +63,6 @@
   (let [grouped (group-by fn coll)]
     (zipmap (keys grouped)
             (map first (vals grouped)))))
-
-(defn update-values [m f & args]
-  (reduce (fn [r [k v]] (assoc r k (apply f v args))) {} m))
-
-(defn update-keys [m f & args]
-  (reduce (fn [r [k v]] (assoc r (apply f k args) v)) {} m))
 
 (defn pluck-text
   ([generators keyname]

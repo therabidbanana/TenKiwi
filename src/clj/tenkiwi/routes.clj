b5975b580516fc51eff59c18a02c67733b5df769
David <therabidbanana@gmail.com>
2021-09-13 02:24:16 +0000

    Support other urls

--- src/clj/tenkiwi/routes.clj
+++ src/clj/tenkiwi/routes.clj
@@ -14,7 +14,39 @@
          io/input-stream
          response
          (update-in [:body] #(str/replace (slurp %) "CSRF_TOKEN" (force csrf/*anti-forgery-token*)))
+         (update-in [:body] #(str/replace % "</head>" (str
+                                                       "
+                                                       <meta property='al:ios:app_name' content='Tenkiwi'>
+                                                       <meta property='al:ios:app_store_id' content='1571524662'>
+                                                       <meta property='al:ios:url' content='/'>"
+                                                       "</head>")))
          (assoc :headers {"Content-Type" "text/html; charset=utf-8"})))
+   (GET "/unlock/:code" _
+        (-> "public/index.html"
+            io/resource
+            io/input-stream
+            response
+            (update-in [:body] #(str/replace (slurp %) "CSRF_TOKEN" (force csrf/*anti-forgery-token*)))
+            (update-in [:body] #(str/replace % "</head>" (str
+                                                          "
+                                                       <meta property='al:ios:app_name' content='Tenkiwi'>
+                                                       <meta property='al:ios:app_store_id' content='1571524662'>
+                                                       <meta property='al:ios:url' content='/'>"
+                                                          "</head>")))
+            (assoc :headers {"Content-Type" "text/html; charset=utf-8"})))
+   (GET "/join/:code" _
+        (-> "public/index.html"
+            io/resource
+            io/input-stream
+            response
+            (update-in [:body] #(str/replace (slurp %) "CSRF_TOKEN" (force csrf/*anti-forgery-token*)))
+            (update-in [:body] #(str/replace % "</head>" (str
+                                                          "
+                                                       <meta property='al:ios:app_name' content='Tenkiwi'>
+                                                       <meta property='al:ios:app_store_id' content='1571524662'>
+                                                       <meta property='al:ios:url' content='/'>"
+                                                          "</head>")))
+            (assoc :headers {"Content-Type" "text/html; charset=utf-8"})))
    (GET "/cards.html" _
         (-> "public/cards.html"
             io/resource
